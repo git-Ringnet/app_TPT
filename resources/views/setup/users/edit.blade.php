1ac5fbc8c78b9a06d18978d848f4ccc60618c325
Nguyễn Quốc Việt <88937231+nqv1710@users.noreply.github.com>
2025-03-07 18:41:55 +0700

    push

--- resources/views/setup/users/edit.blade.php
+++ resources/views/setup/users/edit.blade.php
@@ -85,7 +85,7 @@
                                 <div class="title-info py-2 border border-left-0 border-top-0 height-100">
                                     <p class="p-0 m-0 margin-left32 text-13">Mật khẩu</p>
                                 </div>
-                                <input type="text" placeholder="Nhập thông tin" name="password" autocomplete="off"
+                                <input type="password" placeholder="Nhập thông tin" name="password" autocomplete="off"
                                     class="border border-white height-100 w-100 py-2 border-left-0 border-right-0 border-top-0 px-3 text-13-black bg-input-guest-blue">
                             </div>
                             <div class="d-flex align-items-center height-60-mobile option-radio">
