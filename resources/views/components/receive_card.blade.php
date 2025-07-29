40c4dc0598e56d9c304cf885e8ce7dc0a783ac40
Thái Minh Hiếu <87421130+HieuTamihi@users.noreply.github.com>
2025-07-29 16:08:56 +0700

    update

--- resources/views/components/receive_card.blade.php
+++ resources/views/components/receive_card.blade.php
@@ -84,7 +84,7 @@
                         {{ $receiving->form_type == 3 ? 'checked' : '' }}>
                 </div>
             </div>
-            <p class="info font-italic font-weight-bold">
+            <p class="info font-italic font-weight-bold py-2">
                 Ghi chú: Khi nhận UPS gửi bảo hành, sửa chữa, quý khách cần đem theo phiếu tiếp nhận này. Các trường hợp
                 làm
                 mất, làm rách phiếu sẽ không được giải quyết. Xin cảm ơn.
