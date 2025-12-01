b30ebcf2aaaea9a13646f6da141a3fae7b6167dc
Thái Minh Hiếu <87421130+HieuTamihi@users.noreply.github.com>
2025-12-01 16:15:27 +0700

    update https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Ép toàn bộ link sinh ra phải là HTTPS
+    if($this->app->environment('production') || true) {
+        URL::forceScheme('https');
+    }
         // Ensure Bootstrap pagination view and Vietnamese labels
         Paginator::useBootstrap();
     }
